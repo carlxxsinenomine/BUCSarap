75130a9024606e1cb251dd81c64203fd6863ca40
Gebhel Anselm <gebhel.anselm@gmail.com>
2026-03-15 19:36:34 +0800

    feat: schema (#28)
    * feat: session draft to allow interaction of program and db, and draft user model for set up
    
    * fix: parameters of the id in the models, and changed the port to 3306 MySQL pala kase gamit natin and added docs sa main
    
    * feat: added schema for the API and input validation coming from user side

---
+++ backend/src/schema/user.py
@@ -0,0 +1,18 @@
+# This dictates what should the frontend only request and what backend sends back. 
+from pydantic import BaseModel, Field
+from uuid import UUID 
+
+# Input schema
+class UserCreate(BaseModel):
+    username: str = Field(min_length=3, max_length=50, description="Please enter a valid name")
+    password: str = Field(min_length=8, description="Please enter a strong and valid password")
+
+# Output schema
+class UserResponse(BaseModel):
+    id: UUID
+    username: str
+
+    # Translates default dictionaries to objects
+    #  This prevents app crash 
+    class Config:
+        from_attributes = True
