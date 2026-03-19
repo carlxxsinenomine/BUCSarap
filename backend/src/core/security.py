706d085e5cccf982e90e7f0557baae14e26ac059
anselmusPhytagoras32 <gebhel.anselm@gmail.com>
2026-03-20 01:14:26 +0800

    fix: changed the passlib to bcrypt because passlib is severly outdated

--- backend/src/core/security.py
+++ backend/src/core/security.py
@@ -1,8 +1,16 @@
-from passlib.context import CryptContext
+import bcrypt
 
-password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def hash_password(password: str) -> str:
-    return password_context.hash(password)
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(pwd_bytes, salt)
+    
+    # Decode back to a normal string so it can be saved in the database
+    return hashed_password.decode('utf-8')
 
 def verify_hash(password: str, hashed_pass: str) -> bool:
-    return password_context.verify(password, hashed_pass)
+    # Convert both the login attempt and the database hash into bytes for comparison
+    password_byte_enc = password.encode('utf-8')
+    hashed_pass_byte_enc = hashed_pass.encode('utf-8')
+    
+    return bcrypt.checkpw(password_byte_enc, hashed_pass_byte_enc)
