ae3feb4775c31660c9139450b219de8bbee04303
anselmusPhytagoras32 <gebhel.anselm@gmail.com>
2026-04-25 16:08:36 +0800

    clean: removed models because we use native sql, and renamed the sql file

--- backend/src/models/ProductCategory.py
+++
@@ -1,8 +0,0 @@
-from db.Base import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
-
-class ProductCategory(Base):
-    __tablename__ = "product_category"
-    category_id = Column(Integer, primary_key=True, autoincrement=True)
-    stall_id = Column(Integer, ForeignKey("stall.stall_id"), nullable=False)
-    category_name = Column(String(100), nullable=False)
--- backend/src/models/ProductPile.py
+++
@@ -1,9 +0,0 @@
-from db.Base import Base
-from sqlalchemy import Column, Integer, ForeignKey
-
-class ProductPile(Base):
-    __tablename__ = "product_pile"
-    product_pile_id = Column(Integer, primary_key=True, autoincrement=True)
-    product_id = Column(Integer, ForeignKey("product.product_id"), nullable=False)
-    stall_id = Column(Integer, ForeignKey("stall.stall_id"), nullable=False)
-    product_quantity = Column(Integer, nullable=False)
